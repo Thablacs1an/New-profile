0c8d4097bb1e1ea9f76b2ee234155a254d6cc0df
Thablacs1an <immaaustine94@gmail.com>
2026-03-18 09:56:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("projectBtn");
+
+button.addEventListener("click", function () {
+  alert("Projects section coming soon 🚀");
+
+  // Later you can redirect:
+  // window.location.href = "projects.html";
+});
